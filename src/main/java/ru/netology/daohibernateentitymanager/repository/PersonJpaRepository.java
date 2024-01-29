c58ff3305175b26b1596b7dd54e41f870afc9c82
DanilaChelenkov <chelenkov.dv@gmail.com>
2024-01-29 22:30:01 +0300

    Add @Query way to project

--- src/main/java/ru/netology/daohibernateentitymanager/repository/PersonJpaRepository.java
+++ src/main/java/ru/netology/daohibernateentitymanager/repository/PersonJpaRepository.java
@@ -1,6 +1,9 @@
 package ru.netology.daohibernateentitymanager.repository;
 
+import org.hibernate.query.spi.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.netology.daohibernateentitymanager.model.Person;
 
@@ -9,9 +12,15 @@ import java.util.Optional;
 
 @Repository
 public interface PersonJpaRepository extends JpaRepository<Person, Long> {
-    List<Person> findByCityOfLiving(String city);
 
+    @Query("select p from Person p where p.cityOfLiving=:city")
+    List<Person> findByCityOfLiving(@Param("city") String city);
+
+    @Query("select p from Person p where p.age<=?1 order by p.age asc")
     List<Person> findByAgeLessThanOrderByAgeAsc(Integer age);
 
+    @Query(value = "select p.* from netology_db.person p where p.name=?1 and p.surname=?2 limit 1",
+            nativeQuery = true
+    )
     Optional<Person> findFirstByNameAndSurname(String name, String surname);
 }
